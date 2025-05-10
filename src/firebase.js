d3596c1de58ce64c8bbc1715e102c48f2787b0fa
leviabbu <curosrskyweb@gmail.com>
2025-05-10 21:23:43 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth";
+import { getAuth, connectAuthEmulator } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -21,5 +21,12 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const auth = getAuth(app);
+
+// Add automatic reconnect if auth is interrupted
+auth.onAuthStateChanged((user) => {
+  if (user) {
+    console.log('User authenticated:', user.email);
+  }
+});
 
 export { auth };
